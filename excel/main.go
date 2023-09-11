ea8f2825e2ef59b84ca634f4e23386f2a2b9b708
q10357 <isabel@chaparian.no>
2023-09-12 00:33:09 +0200

    additional changes for my use case

--- excel/main.go
+++ excel/main.go
@@ -4,49 +4,98 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/xuri/excelize/v2"
 )
 
 func main() {
 	fmt.Println("len", len(os.Args))
-	pfileName := "tmp"
-	flag.StringVar(&pfileName, "fileName", "usx", "Filename")
+	pFileName := "tmpFilename"
+	pSheetName := "tmpSheetname"
+	flag.StringVar(&pFileName, "fileName", "usx", "Filename")
+	flag.StringVar(&pSheetName, "sheetName", "Sheet1", "SheetName")
 	//This should also be flag
+	//labels := []string{}
+	//labelCellNumber := [5]string{"A1", "B1"}
+
 	delimiter := ","
+	//count := 0
 
 	flag.Parse()
-	fmt.Println(pfileName)
+	fmt.Println(pFileName)
 
 	/*for _, arg := range os.Args[1:] {
 		fmt.Println(arg)
 	}*/
 
-	f, err := excelize.OpenFile(pfileName + ".xlsx")
+	f, err := excelize.OpenFile(pFileName + ".xlsx")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Get value from cell by given sheet name and axis.
-	/*cellValue, err := f.GetCellValue("Sheet1", "A1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(cellValue)
-	*/
-	// Get all the rows in the sheet.
-	rows, err := f.GetRows("Sheet1")
+	// This will store values from column A based on column B
+	valuesMap := make(map[string][]string)
+
+	rows, err := f.GetRows(pSheetName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	// Iterate over the rows and print the cell values.
 	for _, row := range rows {
-		for _, colCell := range row {
-			fmt.Print(colCell, delimiter)
+		if len(row) >= 2 { // Ensure that we have at least two columns in the current row
+			colAValue := row[0]
+			colBValue := row[1]
+
+			// Add to map based on the value in column B
+			valuesMap[colBValue] = append(valuesMap[colBValue], colAValue)
 		}
 	}
+
+	// Print the map
+	for key, values := range valuesMap {
+		fmt.Printf("Key: %s\n%s\n\n", key, strings.Join(values, delimiter))
+	}
+
+	fmt.Println()
+	//fmt.Printf("\ncount: %d\n", count)
 }
+
+// Get all the rows in the sheet.
+/*cols, err := f.GetCols("Sheet1")
+if err != nil {
+	fmt.Println(err)
+	return
+}
+for _, col := range cols {
+	for _, rowCell := range col {
+		fmt.Print(rowCell, "\t")
+	}
+	fmt.Println()
+}
+
+
+// Get value from cell by given sheet name and axis.
+cellValue, err := f.GetCellValue("Sheet1", labelCellNumber[0])
+if err != nil {
+	fmt.Println(err)
+	return
+}
+fmt.Println(cellValue)
+
+rows, err := f.GetRows("Sheet1")
+if err != nil {
+	fmt.Println(err)
+	return
+}
+fmt.Println("PRINTING OUT CELLS...")
+
+// Iterate over the rows and print the cell values.
+for _, row := range rows {
+	for _, colCell := range row {
+		fmt.Print(colCell, delimiter)
+		count++
+	}
+}*/
